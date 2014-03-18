f38c2d354c8239b774e04c7de23b6a0d5a093e2e
steve-downing <ssdowning@gmail.com>
2014-03-18 14:21:05 -0700

    Using explicit ID types in TestUser

--- src/test/java/org/stevedowning/commons/idutils/TestUser.java
+++ src/test/java/org/stevedowning/commons/idutils/TestUser.java
@@ -1,17 +1,17 @@
 package org.stevedowning.commons.idutils;
 
-public class TestUser implements MutablyIdentifiable<TestUser, Id<TestUser>> {
-    private Id<TestUser> id = null;
+public class TestUser implements MutablyIdentifiable<TestUser, LongId<TestUser>> {
+    private LongId<TestUser> id = null;
     private final String name;
     
     public TestUser(String name) { this.name = name; }
-    public TestUser(String name, Id<TestUser> id) {
+    public TestUser(String name, LongId<TestUser> id) {
         this(name);
         this.id = id;
     }
     
     public String getName() { return name; }
 
-    public Id<TestUser> getId() { return id; }
-    public void setId(Id<TestUser> id) { this.id = id; }
+    public LongId<TestUser> getId() { return id; }
+    public void setId(LongId<TestUser> id) { this.id = id; }
 }
