7282d9aaabcfaaff99c0f9d765235d19f3761eed
Steve Downing <ssdowning@gmail.com>
2014-03-22 18:54:47 -0700

    Templating on Id<? super T> instead of Id<T>

--- src/main/java/org/stevedowning/commons/idutils/datastructures/IdMap.java
+++ src/main/java/org/stevedowning/commons/idutils/datastructures/IdMap.java
@@ -7,17 +7,17 @@ import java.util.Set;
 import org.stevedowning.commons.idutils.Id;
 import org.stevedowning.commons.idutils.Identifiable;
 
-public interface IdMap<T extends Identifiable<T>> {
+public interface IdMap<T extends Identifiable<? super T>> {
     public void clear();
-    public boolean containsKey(Id<T> key);
+    public boolean containsKey(Id<? super T> key);
     public boolean containsValue(T val);
-    public Set<Entry<Id<T>, T>> entrySet();
-    public T get(Id<T> key);
+    public Set<Entry<Id<? super T>, T>> entrySet();
+    public T get(Id<? super T> key);
     public boolean isEmpty();
-    public Set<Id<T>> keySet();
+    public Set<Id<? super T>> keySet();
     public T put(T val);
     public void putAll(Collection<T> vals);
-    public T remove(Id<T> key);
+    public T remove(Id<? super T> key);
     public int size();
     public Collection<T> values();
 }
