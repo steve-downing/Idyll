6d32fe04259da51600c82bef910fe769b0941dce
Steve Downing <ssdowning@gmail.com>
2014-04-07 00:30:33 -0700

    Simplifying the wrapper map

--- src/main/java/org/stevedowning/commons/idyll/datastructures/WrapperIdMap.java
+++ src/main/java/org/stevedowning/commons/idyll/datastructures/WrapperIdMap.java
@@ -9,60 +9,42 @@ import org.stevedowning.commons.idyll.Id;
 import org.stevedowning.commons.idyll.Identifiable;
 import org.stevedowning.commons.idyll.exception.NullIdException;
 
-public abstract class AbstractIdMap<T extends Identifiable<? super T>> implements IdMap<T> {
-    private Map<Id<? super T>, T> map = null;
+public class WrapperIdMap<T extends Identifiable<? super T>> implements IdMap<T> {
+    private final Map<Id<? super T>, T> map;
     
-    protected abstract Map<Id<? super T>, T> getInitialMap();
-    
-    private synchronized void initializeMapIfNecessary() {
-        if (map == null) map = getInitialMap();
+    public WrapperIdMap(Map<Id<? super T>, T> map) {
+        this.map = map;
     }
     
     public Map<Id<? super T>, T> getMap() {
-        initializeMapIfNecessary();
         return map;
     }
 
     public void clear() {
-        if (map != null) map.clear();
+        map.clear();
     }
 
     public boolean containsKey(Id<? super T> key) {
-        if (map == null) {
-            return false;
-        } else {
-            return map.containsKey(key);
-        }
+        return map.containsKey(key);
     }
 
     public boolean containsValue(T val) {
-        if (map == null) {
-            return false;
-        } else {
-            return map.containsValue(val);
-        }
+        return map.containsValue(val);
     }
 
     public Set<Entry<Id<? super T>, T>> entrySet() {
-        initializeMapIfNecessary();
         return map.entrySet();
     }
 
     public T get(Id<? super T> key) {
-        initializeMapIfNecessary();
         return map.get(key);
     }
 
     public boolean isEmpty() {
-        if (map == null) {
-            return true;
-        } else {
-            return map.isEmpty();
-        }
+        return map.isEmpty();
     }
 
     public Set<Id<? super T>> keySet() {
-        initializeMapIfNecessary();
         return map.keySet();
     }
     
@@ -72,32 +54,24 @@ public abstract class AbstractIdMap<T extends Identifiable<? super T>> implement
 
     public T put(T val) {
         if (val.getId() == null) throw new NullIdException();
-        initializeMapIfNecessary();
         return map.put(val.getId(), val);
     }
 
     public void putAll(Collection<T> vals) {
-        initializeMapIfNecessary();
         for (T val : vals) {
             put(val);
         }
     }
 
     public T remove(Id<? super T> key) {
-        initializeMapIfNecessary();
         return map.remove(key);
     }
 
     public int size() {
-        if (map == null) {
-            return 0;
-        } else {
-            return map.size();
-        }
+        return map.size();
     }
 
     public Collection<T> values() {
-        initializeMapIfNecessary();
         return map.values();
     }
 }
