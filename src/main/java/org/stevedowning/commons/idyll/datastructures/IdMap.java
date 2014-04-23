f066175aad859fe5045816d46a774835ca39fd1f
Steve Downing <ssdowning@gmail.com>
2014-04-23 13:48:21 -0700

    Fixing some param names

--- src/main/java/org/stevedowning/commons/idyll/datastructures/IdMap.java
+++ src/main/java/org/stevedowning/commons/idyll/datastructures/IdMap.java
@@ -10,10 +10,11 @@ public interface IdMap<T extends Identifiable<? super T>> extends Iterable<T> {
     public void add(T val);
     public void addAll(Collection<? extends T> vals);
     public void clear();
-    public boolean containsId(Id<? super T> key);
-    public T get(Id<? super T> key);
+    public boolean containsId(Id<? super T> id);
+    public T get(Id<? super T> id);
     public boolean isEmpty();
     public Set<Id<? super T>> getIds();
-    public T remove(Id<? super T> key);
+    public T remove(Id<? super T> id);
     public int size();
+    public Collection<T> values();
 }
