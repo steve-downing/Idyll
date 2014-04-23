74da3456c896d8e1d0f38f3c0e74e5977f3fecf3
Steve Downing <ssdowning@gmail.com>
2014-04-23 13:26:11 -0700

    Removing unnecessary params

--- src/main/java/org/stevedowning/commons/idyll/datastructures/LinkedHashIdMap.java
+++ src/main/java/org/stevedowning/commons/idyll/datastructures/LinkedHashIdMap.java
@@ -1,13 +1,12 @@
 package org.stevedowning.commons.idyll.datastructures;
 
 import java.util.LinkedHashMap;
-import java.util.Map;
 
 import org.stevedowning.commons.idyll.Id;
 import org.stevedowning.commons.idyll.Identifiable;
 
 public class LinkedHashIdMap<T extends Identifiable<? super T>> extends WrapperIdMap<T> {
-    public LinkedHashIdMap(Map<Id<? super T>, T> map) {
+    public LinkedHashIdMap() {
         super(new LinkedHashMap<Id<? super T>, T>());
     }
 }
