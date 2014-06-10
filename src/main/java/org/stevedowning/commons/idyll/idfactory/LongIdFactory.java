7915e13e8de85c03d676c1cc558555ecb1cd3f89
Steve Downing <ssdowning@gmail.com>
2014-06-10 09:35:09 -0700

    Allowing the client to initialize LongIdFactory

--- src/main/java/org/stevedowning/commons/idyll/idfactory/LongIdFactory.java
+++ src/main/java/org/stevedowning/commons/idyll/idfactory/LongIdFactory.java
@@ -6,7 +6,15 @@ import org.stevedowning.commons.idyll.Identifiable;
 import org.stevedowning.commons.idyll.ids.LongId;
 
 public final class LongIdFactory implements IdFactory {
-    private final AtomicLong counter = new AtomicLong(0);
+    private final AtomicLong counter;
+    
+    public LongIdFactory() {
+        this(0);
+    }
+    
+    public LongIdFactory(long initialIdVal) {
+        counter = new AtomicLong(initialIdVal);
+    }
 
     public <T extends Identifiable<T>> LongId<T> generateId() {
         LongId<T> id = new LongId<T>(counter.getAndIncrement());
